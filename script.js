adb8bb556519d3259ee85dd3c0349b6836b05b42
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-03-11 09:27:50 +0300

    updt

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ anime({
   targets: '.path',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
-  duration: 10000,
+  duration: 12000,
   delay: function(el, i) { return i * 250 },
   direction: 'alternate',
   // loop: true
