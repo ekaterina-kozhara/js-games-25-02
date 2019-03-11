1ee95f59cf06d4a0b13d47349df1c99dde4981cc
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-03-11 05:17:08 +0300

    коротаем вечерочки и денечки с плакатиком

--- script.js
+++ script.js
@@ -8,70 +8,78 @@ anime({
   loop: true
 });
 
-anime({
-targets: '.box1',
-translateX: [-30, -10, 2050],
-rotate: -7,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box2',
-translateY: [-30, 25, 2050],
-rotate: 10,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box3',
-translateX: [30, -25, 950],
-rotate: -10,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box4',
-translateY: [20, -10, 950],
-rotate: -20,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box5',
-translateX: [30, 50, 950],
-rotate: 30,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box6',
-translateY: [-30, 20, 950],
-rotate: 13,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box7',
-translateY: [20, 50, 1000],
-rotate: 10,
-duration: 4000,
-endDelay: 1000,
- direction: 'alternate',
-});
-anime({
-targets: '.box8',
-translateX: [-20, 50, 950],
-rotate: -15,
-duration: 4000,
-endDelay: 1000,
-direction: 'alternate',
-});
+// anime({
+// targets: '.holi',
+// translateX: [-30, -10, 2050],
+// rotate: -7,
+// duration: 4000,
+// endDelay: 1000,
+//  direction: 'alternate',
+// });
+// anime({
+// targets: '.food',
+// translateY: [-30, 25, 2050],
+// rotate: 10,
+// duration: 4000,
+// endDelay: 1000,
+//  direction: 'alternate',
+// });
+// anime({
+// targets: '.piano',
+// translateX: [30, -25, 950],
+// rotate: -10,
+// duration: 4000,
+// endDelay: 1000,
+//  direction: 'alternate',
+// });
+// anime({
+// targets: '.feetFlowers',
+// translateY: [20, -10, 950],
+// rotate: -20,
+// duration: 4000,
+// endDelay: 1000,
+//  direction: 'alternate',
+// });
+// anime({
+// targets: '.water',
+// translateX: [30, 50, 950],
+// rotate: 30,
+// duration: 4000,
+// endDelay: 1000,
+//  direction: 'alternate',
+// });
+// // anime({
+// // targets: '.box6',
+// // translateY: [-30, 20, 950],
+// // rotate: 13,
+// // duration: 4000,
+// // endDelay: 1000,
+// //  direction: 'alternate',
+// // });
+// // anime({
+// // targets: '.box7',
+// // translateY: [20, 50, 1000],
+// // rotate: 10,
+// // duration: 4000,
+// // endDelay: 1000,
+// //  direction: 'alternate',
+// // });
+// // anime({
+// // targets: '.box8',
+// // translateX: [-20, 50, 950],
+// // rotate: -15,
+// // duration: 4000,
+// // endDelay: 1000,
+// // direction: 'alternate',
+// // });
+// // anime({
+// // targets: '.box21',
+// // translateX: [-20, 50, 2000],
+// // rotate: -15,
+// // duration: 4000,
+// // endDelay: 1000,
+// // direction: 'alternate',
+// // });
 
 $(function(){
 			$("#blinkingCircle").hide();
